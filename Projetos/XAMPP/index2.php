5519956d35df98ad478e42a5914668c6175372a3
darkila26 <darkila26@gmail.com>
2022-03-30 19:43:44 -0300

    Adds index2.php page and accordion option using Jquery library

---
+++ Projetos/XAMPP/index2.php
@@ -0,0 +1,36 @@
+<html>
+	<head>
+		<title> Site em PHP</title>
+		
+		 <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+  <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
+  <script>
+  $( function() {
+    $( "#accordion" ).accordion();
+  } );
+  </script>
+	</head>
+	
+	<body>
+		
+<div id="accordion">
+  <h3>My Pets</h3>
+	<div>
+		<p>
+	Sofia and Kalissi
+		</p>
+	</div>
+  <h3>My favorite colors</h3>
+	<div>
+		<p>
+	Blue <br>
+	Yellow
+		</p>
+	</div>
+</div>
+	
+	</body>
+	
+</html>
